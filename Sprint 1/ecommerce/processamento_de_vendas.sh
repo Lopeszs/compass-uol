ef8e3eacedf42149d332a935151d660ab1c2c642
Lopeszs <isabelafl07005@gmail.com>
2024-07-31 18:18:26 -0400

    Finalizado o desafio 4.1 do desafio

--- Sprint 1/ecommerce/processamento_de_vendas.sh
+++ Sprint 1/ecommerce/processamento_de_vendas.sh
@@ -13,12 +13,13 @@
 
 # Cria o diretório "vendas" e copia o arquivo "dados_de_vendas.csv" para dentro dele
 mkdir vendas
-cp dados_de_vendas.csv /vendas
-
+chmod u+rwx vendas
+cp dados_de_vendas.csv vendas/
 
 # Entra no diretório "vendas" e cria o subdiretório "backup"
 cd vendas
 mkdir backup
+chmod u+rwx backup
 
 # Altera o formato da data para yyyymmdd e atribui a variável "date_f"
 date_f=$(date +"%Y%m%d")
@@ -31,13 +32,44 @@ cp dados_de_vendas.csv backup/dados-${date_f}.csv
 cd backup
 mv dados-${date_f}.csv backup-dados-${date_f}.csv
 
+# Cria um arquivo de texto
 touch relatorio.txt
 
+# Guarda a data do sistema
+data_sys=$(date +"%Y/%m/%d %H:%M")
 
+# extrai somente a coluna de datas
+somente_datas=$(cut -d',' -f5 "backup-dados-${date_f}.csv" | tail -n +2 | sort -t'/' -k3,3 -k2,2 -k1,1)
 
-#shell separa os comando por ";"
-#echo "escreva aqui" - imprime uma mensagem
-#date - mostra a data
-#sort - ordena informações e imprime
+# Guarda o primeiro registro de vendas
+menor_data=$(echo "$somente_datas" | head -n 1)
+
+# Guarda o último registro de vendas
+maior_data=$(echo "$somente_datas" | tail -n 1)
+
+# Guarda a quantidade de itens diferentes da lista
+qtd_itens=$(cut -d',' -f2 "backup-dados-${date_f}.csv" | tail -n +2 | sort | uniq | wc -l)
+
+# Imprime as informações no arquivo do relatório
+{
+    echo "Data do sistema: $data_sys"
+    echo "Data da primeira venda: $menor_data" 
+    echo "Data da última venda: $maior_data" 
+    echo "Quantidade total de itens diferentes vendidos: $qtd_itens"
+    echo " ===== Primeiras 10 linhas do arquivo de backup dos dados: ====="
+} > relatorio.txt
+
+head -n 10 backup-dados-${date_f}.csv ; head -n 10 backup-dados-${date_f}.csv >> relatorio.txt
+
+# Comprime arquivo de backup dos dados para .zip
+zip -r backup-dados-${date_f}.zip  backup-dados-${date_f}.csv
+
+# Remove o arquivo de backup dos dados
+rm backup-dados-${date_f}.csv
+
+# Volta para o diretório de vendas e apaga o arquivo "dados_de_vendas.csv"
+cd ..
+rm dados_de_vendas.csv
+
 
 #./ ou .processamento_de_vendas - executa o script
